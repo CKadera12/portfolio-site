1ee7ffdb92a252f3dd2aa6f3872bb36a3befa1ce
Clint <kaderaart@gmail.com>
2017-05-07 14:04:46 -0700

    completed appear scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,9 @@
-import Project from "./modules/Project"     
-import WorkLoad from "./modules/WorkLoad" 
+import $ from "jquery";
+import Project from "./modules/Project";  
+import WorkLoad from "./modules/WorkLoad";
+import RevealOnScroll from "./modules/RevealOnScroll"; 
       
 var workLoad = new WorkLoad();
-var project = new Project(); 
- 
+var project = new Project();  
+new RevealOnScroll($(".card"), "90%");
+new RevealOnScroll($(".contact__text-content"), "85%"); 
